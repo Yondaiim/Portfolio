b0597981c0544c028eb0d756d5f17762ad51d7e7
Adam Gallas <adam.gls@hotmail.com>
2023-05-23 19:37:05 +0200

    Portfolio

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Etudiant', 'Passionné', 'Support Informatique'],
+    strings: ['Étudiant', 'Passionné', 'Support Informatique'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
